2caffd81b07bd34c81af634fbbca0ea655b69e94
shreyanshpdixit <shreyanshdixit77@gmail.com>
2024-02-20 10:22:36 +0530

    final

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,3 +1,25 @@
 var output = [];
-output.push(1);
-console.log(output);
+var count = 1;
+
+function fizzBuzz() {
+
+    if(count % 3 === 0 && count % 5 === 0) {
+        output.push("FizzBuzz");
+    }
+    
+    else if(count % 3 === 0) {
+        output.push("Fizz");
+    } 
+    
+    else if(count % 5 === 0) {
+        output.push("Buzz");
+    } 
+    
+    else {
+        output.push(count);
+    }
+    
+    count++;
+    
+    console.log(output);
+}
